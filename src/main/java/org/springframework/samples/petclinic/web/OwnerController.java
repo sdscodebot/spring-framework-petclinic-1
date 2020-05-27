86eb0d843fab629fc40b3082cbbf9001f186cd64
suhyunsong515 <47510558+suhyunsong515@users.noreply.github.com>
2020-05-28 08:56:07 +0900

    Update OwnerController.java

--- src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
@@ -143,8 +143,8 @@ public class OwnerController {
 		}
 	}
 
-	private boolean isInternetTel(String beautyfull) {
-		return beautyfull == "070";
+	private boolean isInternetTel(String beautyful) {
+		return beautyful == "070";
 	}
 	
 	@RequestMapping(value = "/owners/{ownerId}/edit", method = RequestMethod.GET)
